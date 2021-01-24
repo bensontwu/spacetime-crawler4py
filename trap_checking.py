e9426ca8d7fcc8ff42a7d179265b741d070fcca0
Zipz-coder <74122410+Zipz-coder@users.noreply.github.com>
2021-01-24 12:26:15 -0800

    check for path similarity

--- trap_checking.py
+++ trap_checking.py
@@ -2,18 +2,16 @@ from urllib.parse import urlparse
 
 class trap_check:
     def __init__(self):
-        self.tracking_url = ""
+        self.tracking_url_netloc = ""
+        self.tracking_url_path = ""
         self.count = 0
         
     def urlCount(self, url):
         url_parsed = urlparse(url)
-        if url_parsed.netloc != self.tracking_url:
-            if self.tracking_url == "":
-                self.tracking_url = url_parsed.netloc
-                self.count += 1
-            else:
-                self.tracking_url = url_parsed.netloc
-                self.count = 1
+        if (url_parsed.netloc != self.tracking_url_netloc or url_parsed.path != self.tracking_url_path):
+            self.tracking_url_netloc = url_parsed.netloc
+            self.tracking_url_path = url_parsed.path
+            self.count = 1
         else:
             self.count += 1
     
@@ -27,33 +25,31 @@ class trap_check:
         return self.count
 
 
-
 #Testing the class
-
-#There are 27 links in the url_list with the same authority, it should stop at 20.
 url_list = ["http://www.ics.uci.edu", "https://www.ics.uci.edu/grad/admissions/index", "https://www.ics.uci.edu/about/search/index.php",
-            "https://www.ics.uci.edu/community/alumni/", "https://www.ics.uci.edu/community/alumni/", "https://www.ics.uci.edu/community/alumni/",
-            "https://www.ics.uci.edu/community/alumni/", "https://www.ics.uci.edu/community/alumni/", "https://www.ics.uci.edu/community/alumni/",
-            "https://www.ics.uci.edu/community/alumni/", "https://www.ics.uci.edu/community/alumni/", "https://www.ics.uci.edu/community/alumni/",
-            "https://www.ics.uci.edu/community/alumni/", "https://www.ics.uci.edu/community/alumni/", "https://www.ics.uci.edu/community/alumni/",
-            "https://www.ics.uci.edu/community/alumni/", "https://www.ics.uci.edu/community/alumni/", "https://www.ics.uci.edu/community/alumni/",
-            "https://www.ics.uci.edu/community/alumni/", "https://www.ics.uci.edu/community/alumni/", "https://www.ics.uci.edu/community/alumni/",
-            "https://www.ics.uci.edu/community/alumni/", "https://www.ics.uci.edu/community/alumni/", "https://www.ics.uci.edu/grad/admissions/index",
-            "https://www.ics.uci.edu/grad/admissions/index", "https://www.ics.uci.edu/grad/admissions/index", "https://www.ics.uci.edu/grad/admissions/index"]
+            "https://www.ics.uci.edu/about/search/index.php", "https://www.ics.uci.edu/about/search/index.php", "https://www.ics.uci.edu/about/search/index.php",
+            "https://www.ics.uci.edu/about/search/index.php", "https://www.ics.uci.edu/about/search/index.php", "https://www.ics.uci.edu/about/search/index.php",
+            "https://www.ics.uci.edu/about/search/index.php", "https://www.ics.uci.edu/about/search/index.php", "https://www.ics.uci.edu/about/search/index.php",
+            "https://www.ics.uci.edu/about/search/index.php", "https://www.ics.uci.edu/about/search/index.php", "https://www.ics.uci.edu/about/search/index.php",
+            "https://www.ics.uci.edu/about/search/index.php#ffsf", "https://www.ics.uci.edu/about/search/index.php#fsff", "https://www.ics.uci.edu/about/search/index.php",
+            "https://www.ics.uci.edu/about/search/index.php", "https://www.ics.uci.edu/about/search/index.php", "https://www.ics.uci.edu/about/search/index.php",
+            "https://www.ics.uci.edu/about/search/index.php"]
 
 t = trap_check()
-
+print()
+print("--Testing urls with the same domain--")
+print(f"Number of links in test list: {len(url_list)}")
+print()
 for i in url_list:
     t.urlCount(i)
     if t.highCount() == True:
-            print("It's a trap!")
-            count = t.returnCount()
-            print(f"Number of pages at stop: {count}")
-            break
+        print("20 consecutive links with the same netloc and path. It's a trap!")
+        count = t.returnCount()
+        print(f"Number of links at stop: {count}")
+        break
 
 
-    
 
 
-   
-    
+
+
