5e9a733038fc2bbc74d8419a1445aadbf5ce3fe2
benson <bensontwu@benson-mbp.local>
2021-01-24 14:45:22 -0800

    Add trap check

--- scraper.py
+++ scraper.py
@@ -2,6 +2,7 @@ import re
 from urllib.parse import urlparse
 
 from utils.soup import get_soup
+from utils.trap_check import TrapCheck
 
 def scraper(url, resp):
     links = extract_next_links(url, resp)
@@ -14,8 +15,16 @@ def extract_next_links(url, resp):
         return list()
 
     next_links = []
+    trap_check = TrapCheck()
     for link in soup.find_all('a'):
         href = link.get('href')
+
+        # increment the trap checker
+        trap_check.urlCount(href)
+        if trap_check.highCount():
+            # get rid of all of the trap urls
+            next_links = list(filter(trap_check.is_not_trap_url, next_links))
+            break
         if href != url:
             next_links.append(href)
 
@@ -28,21 +37,18 @@ def is_valid(url):
             return False
 
         if re.match(
-            r"..(css|js|bmp|gif|jpe?g|ico"
+            r".*\.(css|js|bmp|gif|jpe?g|ico"
             + r"|png|tiff?|mid|mp2|mp3|mp4"
             + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
             + r"|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names"
             + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
             + r"|epub|dll|cnf|tgz|sha1"
             + r"|thmx|mso|arff|rtf|jar|csv"
-                + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower()) is None:
-
-            if (re.match(r".*((\.|)ics\.uci\.edu|(\.|)cs\.uci\.edu|(\.|)informatics\.uci\.edu|(\.|)stat\.uci\.edu" +
-                         r"|today\.uci\.edu/department/information_computer_sciences)", url) is not None):
-                return True
+                + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower()):
             return False
-        return False
 
+        return (re.match(r".*((\.|)ics\.uci\.edu|(\.|)cs\.uci\.edu|(\.|)informatics\.uci\.edu|(\.|)stat\.uci\.edu" +
+                         r"|today\.uci\.edu/department/information_computer_sciences)", parsed.netloc))
     except TypeError:
-        print ("TypeError for ", parsed)
+        print("TypeError for ", parsed)
         raise
