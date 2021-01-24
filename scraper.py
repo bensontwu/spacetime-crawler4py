3d2c492bde765d7c8f50acb2f52d2a23534b0fdf
benson <bensontwu@benson-mbp.local>
2021-01-23 23:15:52 -0800

    Added valid check

--- scraper.py
+++ scraper.py
@@ -26,15 +26,21 @@ def is_valid(url):
         parsed = urlparse(url)
         if parsed.scheme not in set(["http", "https"]):
             return False
-        return not re.match(
-            r".*\.(css|js|bmp|gif|jpe?g|ico"
+
+        if re.match(
+            r"..(css|js|bmp|gif|jpe?g|ico"
             + r"|png|tiff?|mid|mp2|mp3|mp4"
             + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
             + r"|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names"
             + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
             + r"|epub|dll|cnf|tgz|sha1"
             + r"|thmx|mso|arff|rtf|jar|csv"
-            + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower())
+                + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower()) is None:
+
+            if (re.match(r".(.ics.uci.edu|.cs.uci.edu|.informatics.uci.edu|.stat.uci.edu" +
+                         r"|today.uci.edu/department/information_computer_sciences)", url) is not None):
+                return True
+        return False
 
     except TypeError:
         print ("TypeError for ", parsed)
