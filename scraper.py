4dafce7dcfbcd9c74a9d090a58322c8c8c793df1
Yu821 <funthinkenjoy@gmail.com>
2021-01-30 10:47:05 +0900

    updated simhash

--- scraper.py
+++ scraper.py
@@ -119,6 +119,119 @@ def is_valid(url):
 		raise
 
 
+from collections import Counter
+import hashlib
+
+def simhash(tokens):
+
+	# list to store tokens(words) in binary format
+	# Ex: ['010101','010101']
+	binary_tokens = list()
+
+
+	for token in tokens:
+		# Convert token(word) to binary string
+		# Ex: "hello" → b'G\xbc\xe5\xc7OX\x9fHg\xdb\xd5~\x9c\xa9\xf8\x08'
+		ida = hashlib.md5(token.encode()).digest()
+
+		# list(ida) converts binary string to binary code (decimal) that ranges from 0~255
+		# Ex: "hello" →  b'G\xbc\xe5\xc7OX\x9fHg\xdb\xd5~\x9c\xa9\xf8\x08 → [165, 245, 12, 2, 54]
+		ida = list(ida)
+
+		# Convert decimal to binary representation consisting of 0 and 1 by f'{i:08b}'
+		# Ex: 165 → 0001010010111001 so the list will be something like ['001010010111001', '111010010110001',...]
+		lst = [f'{i:08b}' for i in ida]
+
+		print(lst)
+
+		# Join each character byte into token byte
+		# Ex:['001010010111001', '111010010110001',...] → '001010010111001111010010110001'
+		# This will become Ex: hello = '001010010111001111010010110001'
+		binary_tokens.append(''.join(lst))
+
+	# Dictionary of binary_tokens and its frequency in token list
+	# Ex: {token(word)='001010010111001111010010110001' : frequency=2}
+	# This will be used for weights
+	freq_dict = dict(Counter(binary_tokens))
+
+	# list of total for each 16 bits
+	tot_lst = list()
+
+	total=0
+
+	# Conduct mathmatical operations
+	# If bit is 1, add weight w. If bit is 0, substract weight w.
+	# range(0,16) because token is represented as 16 bits
+	for i in range(0,16):
+		for j in range(0, len(binary_tokens)):
+
+			# Decide weight from token frequency
+			w = int(freq_dict[binary_tokens[j]])
+
+			if int(binary_tokens[j][i]) == 1:
+				total += w
+			else:
+				total -= w
+		tot_lst.append(total)
+
+		# print(tot_lst)
+
+	finger_print = [1 if i >= 0 else 0 for i in tot_lst]
+
+	return finger_print
+
+
+def similarity(sim1, sim2):
+
+	num_same = 0
+
+	# Calculate number of same elements
+	# Ex: 0110000000000000 and 0110000000000000 has sixteen same elements 
+	for i in range(0,16):
+		if sim1[i] == sim2[i]:
+			num_same+=1
+
+
+	print("Similarity: ", num_same/16)
+
+
+sim1 = simhash(["Hello~!","welcome","to","another","lecture","of","information","retrieval"])
+sim2 = simhash(["Hello~!","welcome","to","another","lecture","of","information","retrieva"])
+
+
+# sim1 = simhash(["aa"])
+# sim2 = simhash(["ab"])
+
+print("sim1 is: ", sim1)
+print("sim2 is: ", sim2)
+
+similarity(sim1, sim2)
+
+
+# tokens=['aa', 'bbb']
+
+# max_len = len(max(tokens, key=len))
+
+
+# token = "bbaeeeea"
+
+
+# ida = hashlib.md5(token.encode()).digest()
+
+# # list(ida) converts binary string to binary code (decimal) that ranges from 0~255
+# # Ex: "hello" →  b'G\xbc\xe5\xc7OX\x9fHg\xdb\xd5~\x9c\xa9\xf8\x08 → [165, 245, 12, 2, 54]
+# ida = list(ida)
+
+# lst = [f'{i:08b}' for i in ida]
+
+# print(lst)
+
+# print(len(ida))
+
+
+
+# lst = [f"{int:b}" for ]
+
 # import urllib.robotparser
 
 # def is_valid_url(url):
@@ -142,14 +255,15 @@ def is_valid(url):
 # 			print("Robots.txt doesn't exist for ", parsed.scheme + "://" + parsed.netloc)
 
 
-urls = ["https://wics.ics.uci.edu/events/", "https://wics.ics.uci.edaaaa/events/", 
-"https://www.ics.uci.edu/community/news/", "https://support.ics.uci.edu/robots.txt",
-"https://support.ics.uci.edu/"]
+# urls = ["https://wics.ics.uci.edu/events/", "https://wics.ics.uci.edaaaa/events/", 
+# "https://www.ics.uci.edu/community/news/", "https://support.ics.uci.edu/robots.txt",
+# "https://support.ics.uci.edu/"]
 
-# There is error for https://support.ics.uci.edu/robots.txt
+# # There is error for https://support.ics.uci.edu/robots.txt
 
-for url in urls:
-	print(is_valid(url))
+# for url in urls:
+# 	print(is_valid(url))
+
 
 # parsed = urlparse(url)
 
